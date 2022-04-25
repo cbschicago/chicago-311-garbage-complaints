0f8eb4913294c1deb63ffcfda2dc72600b21dc4c
hackerlikecomputer <hackerlikecomputer@gmail.com>
2022-04-25 15:04:36 -0500

    fix typo in script

--- src/aggregate_data_by_column.py
+++ src/aggregate_data_by_column.py
@@ -5,4 +5,9 @@ import pandas as pd
 
 df = pd.read_csv(sys.argv[1], low_memory=False)
 
-print(df.groupby(sys.argv[2]).sr_number.nunique().to_frame("unique_service_requests"))
+print(
+    df.groupby(sys.argv[2])
+    .sr_number.nunique()
+    .to_frame("unique_service_requests")
+    .to_csv(index=False, line_terminator="\n")
+)
